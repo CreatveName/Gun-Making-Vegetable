23aae6378d7fa40fbe8d8150f7a05f245ad11d0a
CreatveName <trentku10@gmail.com>
2024-04-27 21:06:36 -0700

    Movemnt + Shooting

---
+++ GunEatingVegetable/Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //public int playerNumber = 1; //If we want multiplayer, not rly needed
+    public float moveSpeed;
+    private Vector2 movement;
+    [SerializeField]
+    private Rigidbody2D player;
+    private string hAxis;
+    private string vAxis;
+    private bool isRunning;
+
+
+    void Start() //Setup Local Multiplayer controls by getting rid of comment + adding in multiple inputs in the unity editor...
+    {
+        hAxis = "Horizontal"; //+ playerNumber;
+        vAxis = "Vertical"; //+ playerNumber;
+    }
+    private void Awake() //Turn on if we want running
+    {
+        //isRunning = true;
+    }
+    // Tracks the players X and Y values and updates it constantly.
+    void Update() //Tracks input from the different axis into a variable
+    {
+        movement.x = Input.GetAxisRaw(hAxis);
+        movement.y = Input.GetAxisRaw(vAxis);
+        /*                                                          Running code
+        if(Input.GetKeyDown(KeyCode.LeftShift) && isRunning)
+        {
+            moveSpeed = moveSpeed/3;
+            isRunning = false;
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning = true;
+            moveSpeed = charStats.moveSpeed;
+        }
+        */
+    }
+    // With the new location of X and Y it allows the character to move based off of it's base movespeed.   
+    private void FixedUpdate() //Physics based movement with inputs * by moveSpeed variable
+    {
+        player.AddForce(new Vector2(movement.x, movement.y) * moveSpeed, ForceMode2D.Force);
+    }
+
+}
---
+++ GunEatingVegetable/Assets/Scripts/Shoot.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject bulletPre;
+    public float bulletSpeed;
+    private float lastFire;
+    public float fireDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float shootHor = Input.GetAxis("ShootHorizontal");
+        float shootVer = Input.GetAxis("ShootVertical");
+        if((shootHor != 0 || shootVer != 0) && Time.time > lastFire + fireDelay)
+        {
+            PewPew(shootHor, shootVer);
+            lastFire = Time.time;
+        }
+    }
+
+    void PewPew(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletPre, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+            (x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
+            (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed,
+            0
+        );//CREATING CONSTANT SPEED FOR OUR BULLETS...
+    }
+}
