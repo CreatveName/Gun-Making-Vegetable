a9cdcabcfcc1d553661c3d87d7f07120b4be788d
CreatveName <trentku10@gmail.com>
2024-04-29 22:18:09 -0700

    button stuff

--- GunEatingVegetable/Assets/Scripts/PlayerCollisions.cs
+++ GunEatingVegetable/Assets/Scripts/PlayerCollisions.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class PlayerCollisions : MonoBehaviour
 {
+    public bool onSell = false;//MAKE BETTER LATER AM TIRED.............
+    public bool onUpgrade = false;
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
 
         if(other.gameObject.tag == "Sell")
         {
-            
+            onSell = true;
+        }
+        if(other.gameObject.tag == "Upgrade")
+        {
+            onUpgrade = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+
+        if(other.gameObject.tag == "Sell")
+        {
+            onSell = false;
+        }
+        if(other.gameObject.tag == "Upgrade")
+        {
+            onUpgrade = false;
         }
     }
 }
---
+++ GunEatingVegetable/Assets/Scripts/UseVeg.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseVeg : MonoBehaviour
+{
+    private PlayerHealth pHealth;
+    private PlayerCollisions playerCollisions;
+    public GameObject sellButton, upgradeButton;
+    
+    private void Awake() 
+    {
+        pHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+        playerCollisions = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCollisions>();
+    }
+    private void Update() //FIX LATER TOO TIRED
+    {
+        if(playerCollisions.onSell == false)
+        {
+            sellButton.SetActive(false);
+        }else{
+            sellButton.SetActive(true);
+        }
+        if(playerCollisions.onUpgrade == false)
+        {
+            upgradeButton.SetActive(false);
+        }else{
+            upgradeButton.SetActive(true);
+        }
+    }
+    public void EatVeggie()
+    {
+        pHealth.playerHP++ ;
+        Destroy(gameObject);
+    }
+    public void SellVeggie()
+    {
+        if(playerCollisions.onSell == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void UpgradeVeggie()
+    {
+        if(playerCollisions.onUpgrade == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
