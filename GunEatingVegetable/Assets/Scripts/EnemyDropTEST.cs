1474b7ae7b0ac77d4ee2f612c8b44381db642f35
CreatveName <trentku10@gmail.com>
2024-04-29 21:06:06 -0700

    RNG for drops

--- GunEatingVegetable/Assets/Scripts/EnemyDropTEST.cs
+++ GunEatingVegetable/Assets/Scripts/EnemyDropTEST.cs
@@ -8,7 +8,7 @@ public class EnemyDropTEST : MonoBehaviour
     public int maxHP;
     public int knockbackForce;
     private Vector3 moveDir;
-    public GameObject dropTest;
+    public GameObject[] dropTest;
     public Rigidbody2D enemy;
 
     private void Start() 
@@ -36,7 +36,19 @@ public class EnemyDropTEST : MonoBehaviour
 
     private void Dead()
     {
-        Instantiate(dropTest, this.transform.position, this.transform.rotation);
+        //int r = Random.Range(0, dropTest.Length);
+        Instantiate(dropTest[GetRandomValue()], this.transform.position, this.transform.rotation);
         Destroy(gameObject);
+    }
+    private int GetRandomValue()
+    {
+        float rand = Random.value;
+        if(rand <= .5f)
+            return 0;
+        if(rand <= .85f)
+            return 1;
+        
+        return 2;
+        
     }
 }
