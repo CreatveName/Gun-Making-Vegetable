a94a12578b7550f2005a954f032b6d2568b05e1d
CreatveName <trentku10@gmail.com>
2024-04-30 17:07:30 -0700

    Pea Upgrade Doneish

--- GunEatingVegetable/Assets/Scripts/PeaShoot.cs
+++ GunEatingVegetable/Assets/Scripts/PeaShoot.cs
@@ -8,39 +8,31 @@ public class PeaShoot : MonoBehaviour
     public float bulletSpeed;
     private float lastFire;
     public float fireDelay;
-    private float shootHor;
-    public float shootVer;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        shootHor = Input.GetAxis("ShootHorizontal");
-        shootVer = Input.GetAxis("ShootVertical");
-        PewPew();
-    }
-
-    void PewPew()
-    {
+        float shootHor = Input.GetAxis("ShootHorizontal");
+        float shootVer = Input.GetAxis("ShootVertical");
         if((shootHor != 0 || shootVer != 0) && Time.time > lastFire + fireDelay)
         {
-            CreateBullet(new Vector3(5f, 0f, 0f));
-            CreateBullet(new Vector3(0f, 0f, 0f));
-            CreateBullet(new Vector3(-5f, 0f, 0f));
-
+            PewPew(shootHor, shootVer, .8f);
+            PewPew(shootHor, shootVer, .9f);
+            PewPew(shootHor, shootVer, 1f);
             lastFire = Time.time;
         }
     }
 
-    void CreateBullet(Vector3 offsetRotation)
+    void PewPew(float x, float y, float z)
     {
-        GameObject bulletClone = Instantiate(bulletPre, transform.position, transform.rotation) as GameObject;
-        bulletClone.transform.Rotate(offsetRotation);
-        Destroy(bulletClone, 1f);
+        Vector3 rand = new Vector3(z, 0f, 0f);
+        GameObject bullet = Instantiate(bulletPre, transform.position, transform.rotation) as GameObject;
+        bullet.transform.Rotate(rand);
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+            (x < 0) ? Mathf.Floor(x) * bulletSpeed * z : Mathf.Ceil(x) * bulletSpeed * z,
+            (y < 0) ? Mathf.Floor(y) * bulletSpeed * z : Mathf.Ceil(y) * bulletSpeed * z,
+            0
+        );//CREATING CONSTANT SPEED FOR OUR BULLETS...
     }
+    
 }
