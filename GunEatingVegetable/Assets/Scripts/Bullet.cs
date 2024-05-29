8e9f5606852cabc27e8348e65b32c6ec433aac94
CreatveName <trentku10@gmail.com>
2024-05-29 16:19:47 -0700

    ui + level layout + bullet

--- GunEatingVegetable/Assets/Scripts/Bullet.cs
+++ GunEatingVegetable/Assets/Scripts/Bullet.cs
@@ -36,4 +36,9 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        Destroy(gameObject);
+    }
 }
